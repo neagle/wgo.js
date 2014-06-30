829061565678f8489b0b02e736c04e707a8aca05
Nate Eagle <nate@nateeagle.com>
2014-06-30 15:20:41 -0400

    Add minification to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var dataUri = require('gulp-data-uri');
 var autoprefix = require('gulp-autoprefixer');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglifyjs');
 var livereload = require('gulp-livereload');
 
 gulp.task('themes-sass', function () {
@@ -24,6 +25,24 @@ gulp.task('themes-sass', function () {
 			path.basename = 'wgo.player.' + path.basename;
 		}))
 		.pipe(gulp.dest('./themes'));
+});
+
+gulp.task('min-js', function () {
+	return gulp.src([
+		'kifu.js',
+		'sgfparser.js',
+		'player.js',
+		'basicplayer.js',
+		'basicplayer.component.js',
+		'basicplayer.infobox.js',
+		'basicplayer.commentbox.js',
+		'basicplayer.control.js',
+		'player.editable.js',
+		'scoremode.js',
+		'player.permalink.js'
+	], { cwd: 'wgo/src' })
+		.pipe(uglify('wgo.min.js'))
+		.pipe(gulp.dest('wgo'));
 });
 
 gulp.task('watch', function () {
